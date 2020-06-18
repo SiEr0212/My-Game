c663e2bdb7f1d8a94eb87ed66b7577da10fa352c
SiEr0212 <simonertel@gmx.de>
2020-06-18 15:08:53 +0200

    added new start and end screen

--- main.js
+++ main.js
@@ -1,36 +1,79 @@
+
+let startGame =false;
+let awesomeSound;
+let gameOver = false;
+let collisionSound;
+let startingImg;
+
 const game = new Game();
+
 // these thre function are p5 created and they are being called from p5 library! :)
 function preload() {
 game.preloadGame();
-  
+startingImg = loadImage("ASSETS/START/startIMG.jpg");
 }
+
 function windowResized() {
   resizeCanvas(windowWidth, windowHeight);
 }
+
 function setup() {
   
   createCanvas(windowWidth, windowHeight);
   textSize(50);
   game.setup();
-  mode = 0;
+  //mode = 0;
 }
 function draw() {
-  clear();
-  if(mode===0){
-    text("Press enter to start", 50%windowWidth,90%windowHeight);
   
-  }
-  if(mode===2){
-    text("GAME OVER     -      Press enter to play again", 50%windowWidth,90%windowHeight);
-    game.player.x = game.player.x + 10;
+  //clear();
+  
+  //if(startGame == false){
+    //image(startingImg, 0, 0, width, height);
+    //textSize(100);
+    //textAlign(CENTER);
+    //text("Please press any key to start! ", width / 2, height / 2);
 
+  //}else
+  //if(mode===2){
+    //text("GAME OVER     -      Press enter to play again", 50%windowWidth,90%windowHeight);
+    //game.player.x = game.player.x + 10;
+
+  //}
+  //if (keyIsDown(13)){
+    //mode=1;
+  //}
+  if (gameOver == true) {
+    image(startingImg, 0, 0, width, height);
+    textSize(100);
+    textAlign(CENTER);
+    text("GAME OVER ", width / 2, height / 2);
+    text("Press any key to play again");
+    fill("white");
+    
+
+    //fill("red");
+    //rect(0, 0, width, height);
+    //awesomeSound.pause();
+  } else {
+    if (startGame == true) {
+      game.drawingGame();
+    } else {
+      // if the game has not started we draw other things like and image or rectangle
+      image(startingImg, 0, 0, width, height);
+      textSize(70);
+      textAlign(CENTER);
+      text("Please press any key to start! ", width / 2, height / 2);
+      fill("white");
+    }
   }
-  if (keyIsDown(13)){
-    mode=1;
-  }
-  if (mode ==1){
-  textSize(200);
-  game.drawingGame();
+
+
+
+
+  //if (mode ==1){
+  //textSize(200);
+  //game.drawingGame();
   if(keyIsDown(32)){
     game.player.jump();
  }
@@ -43,16 +86,16 @@ function draw() {
    game.player.moveBackwards();
  }
   if(game.player.x < 90){
-    text("GAME OVER", 400, 600);
-    textSize(50);
-    mode = 2;
+    gameOver = true;
+  }
+  
   
     
 
   
     
     //gameOver();
-  }
+  
   if (game.player.x < 0){
     game.player.x = game.player.x + 90;
   }
@@ -66,6 +109,21 @@ function draw() {
     game.player.y = game.player.y - 90;
   }}
   
+function keyPressed() {
+  if (startGame == false) {
+    //awesomeSound.play();
+  }
+  // here we set the startGame to true if we press something
+  startGame = true;
+
+  if (keyCode == 32 && startGame == true) {
+    game.player.jump();
+  }
 }
+// if you want to implement some button we can also create a event listener to set the starting Game to true
+//document.querySelector("#something").onclick = function () {
+  //startGame = true;
+//};
+
 
 
